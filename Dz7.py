91a39ebc0bf51445c1eb3ced744466bc946a2783
vip-dacha86 <vip-dacha86@yandex.ru>
2023-05-23 09:53:38 +0500

    DZ_7

--- Dz7.py
+++ Dz7.py
@@ -48,3 +48,24 @@ def print_operation_table(operation, num_rows=6, num_columns=6):
 
 
 print_operation_table(lambda x, y: x * y)
+
+# задача 51
+# Напишите функцию same_by(characteristic, objects), которая проверяет, все ли объекты имеют одинаковое значение некоторой характеристики,
+#  и возвращают True, если это так. Если значение характеристики для разных объектов отличается - то False.
+#   Для пустого набора объектов, функция должна возвращать True. Аргумент characteristic - это функция, которая принимает объект и
+# вычисляет его характеристику.
+# Ввод:                                                       Вывод:
+# values = [0, 2, 10, 6]
+# same if same_by(lambda x: x % 2, values):                    same
+#         print(‘same’) 
+# else:
+#          print(‘different’)
+               
+#      Решение:
+ values = [0, 2, 10, 6] 
+def same_by(func, collection):
+    return(len(list(filter(func, collection)))) == 0
+if same_by(lambda x: x % 2, values):
+    print('same')
+else:
+    print('different')              
